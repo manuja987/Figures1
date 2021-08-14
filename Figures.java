b878182d9a426a9a92d14475ede45a877e44aa2f
manuja987 <manujavernekar1999@gmail.com>
2021-08-14 22:07:17 +0530

    commit1

---
+++ Figures.java
@@ -0,0 +1,52 @@
+
+
+class shape{
+    
+    public double width;
+    public double height;
+    
+    void disp(){
+        System.out.println("width of shape="+width+"  "+"height of shape="+height);
+    }
+}
+
+class triangle extends shape{
+    
+    void disp(){
+        System.out.println("width="+width+"  "+"height="+height+ " of a triangle");
+    }
+    void area(){
+        System.out.println("area of triangle= "+(width*height/2));
+        
+    }
+}
+
+class rectangle extends shape{
+    void area(){
+        System.out.println("area of triangle= "+(width*height));
+        
+    }
+}
+class Figures
+{
+   public static void main(String args[])
+   {
+       triangle t = new triangle();
+       rectangle r = new rectangle();
+       t.width=10;
+       t.height=20;
+       r.width=10;
+       r.height=20;
+	/*try{
+       Thread.sleep(100000);}
+	catch(InterruptedException ie)
+{
+	System.out.println("scanning");}*/
+
+       t.disp();
+       t.area();
+       r.disp();
+       r.area();
+       
+   }
+}
